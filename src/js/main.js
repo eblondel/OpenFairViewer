f13395c1bb12656b4933b6bb7c839d00d7033d13
eblondel <emmanuel.blondel1@gmail.com>
2020-02-26 15:13:32 +0100

    update main.js

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,5 @@
 /**
- * SDI Lab data viewer (example of configuration for OpenFairViewer)
+ * SDI Lab data viewer 
  *
  * @author Emmanuel Blondel GIS & Marine web-information systems Expert
  *		   Contact: https://eblondel.github.io / emmanuel.blondel1@gmail.com 
@@ -12,33 +12,32 @@ $(document).ready(function(){
 	app = new OpenFairViewer({
 		OGC_CSW_BASEURL: "http://localhost:8080/geonetwork/srv/eng/csw"
 	},{
-		catalog : {
-			filters: []
+		browse : {
+			filters: [],
+			filterByWMS: true,
+			datasetInfoHandler : function(metadata){
+				//var datasetInfoUrl = "https://geonetwork.d4science.org/geonetwork/srv/en/main.home?uuid=" + metadata.fileIdentifier;
+				var datasetInfoUrl = "https://geonetwork-sdi-lab.d4science.org/geonetwork/srv/eng/catalog.search#/metadata/" + metadata.fileIdentifier;
+				$('#datasetInfo').empty().html('<iframe src="'+datasetInfoUrl+'" style="overflow: hidden; height: 100%; width: 100%; position: absolute;"> frameborder="0" marginheight="0"></iframe>');
+				app.openInfoDialog();
+			}
+		},
+		query: {
+			time: 'slider'
 		},
 		map : {
 			extent: [-180, -90, 180, 90],
 			zoom: 3,
+			styling : { dynamic : true },
 			layergroups : [{name: "Base overlays"},{name: "My SDI Lab maps"}],
 			mainlayergroup: 1,
 			overlays: [
 				{
 					group: 0, id: "fsa", title: "FAO major areas & breakdown",
-					wmsUrl: "http://www.fao.org/figis/geoserver/area/wms", layer: "area:FAO_AREAS",
+					wmsUrl: "http://www.fao.org/figis/geoserver/fifao/wms", layer: "fifao:FAO_AREAS_CWP",
 					visible: false, showLegend: true, opacity: 0.9, tiled: true, cql_filter: undefined
 				}
 			]
-		},
-		ui 	: {
-			browse: {
-				filterByWMS: false,
-				filterByContentInfo: false,
-				datasetInfoHandler : function(metadata){
-					var datasetInfoUrl = "http://localhost:8080/geonetwork/srv/eng/catalog.search#/metadata/" + metadata.fileIdentifier;
-					$('#datasetInfo').empty().html('<iframe src="'+datasetInfoUrl+'" style="overflow: hidden; height: 100%; width: 100%; position: absolute;"> frameborder="0" marginheight="0"></iframe>');
-					app.openInfoDialog();
-				}
-			},
-			query: { time: 'slider'}
 		}
 	});
 	app.init();
