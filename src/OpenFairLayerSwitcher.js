f4b2f27cb84eb0d4f1de3346ce071a2becb5b901
eblondel <emmanuel.blondel1@gmail.com>
2020-11-09 15:48:20 +0100

    improve legend css

--- src/OpenFairLayerSwitcher.js
+++ src/OpenFairLayerSwitcher.js
@@ -28,9 +28,10 @@ export default class OpenFairLayerSwitcher extends LayerSwitcher {
 				var img = new Image();
 				img.src = imgSrc;
 				var smallLegend = img.width <= 32 && img.height <= 20;
-				var smallLegendCss = smallLegend? 'margin-right:10px;float:right;' : 'margin-left:40px;';
+				var legendCss = 'margin-left:40px;';
+				//var smallLegendCss = smallLegend? 'margin-right:10px;float:right;' : 'margin-left:40px;';
 				legend.style.display = (lyr.getVisible()? (smallLegend? "block": "table") : "none");
-				var img = '<img src="'+imgSrc+'" style="'+smallLegendCss+'"/>';
+				var img = '<img src="'+imgSrc+'" style="'+legendCss+'"/>';
 				legend.innerHTML = img;
 				li.appendChild(legend);
 			}
