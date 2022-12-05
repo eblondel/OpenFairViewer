243db6391c45ab35a3c1ab3f6cdbcfe1f8909174
eblondel <emmanuel.blondel1@gmail.com>
2022-12-05 15:52:45 +0100

    test patch for ol-cesium

--- src/OpenFairViewer.js
+++ src/OpenFairViewer.js
@@ -138,7 +138,7 @@ class OpenFairViewer {
 		var this_ = this;
 		
 		//version
-		this.versioning = {VERSION: "2.7.8", DATE: new Date('2022-12-01')}
+		this.versioning = {VERSION: "2.7.8.9001", DATE: new Date('2022-12-05')}
 		
 		//protocol
 		this.protocol = window.origin.split("://")[0];
