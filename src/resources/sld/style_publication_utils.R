b86233b5e90aeb89025b47446986967ab60c9bc5
eblondel <emmanuel.blondel1@gmail.com>
2024-01-17 11:13:20 +0100

    update procedure for SLD styles management

--- src/resources/sld/style_publication_utils.R
+++ src/resources/sld/style_publication_utils.R
@@ -1,4 +1,12 @@
 library(geosapi)
 GS = GSManager$new(url = "{{GEOSERVER_URL}}", user = "{{GEOSERVER_USER}}", pwd = "{{GEOSERVER_PASSWORD}}", logger = "INFO")
-files = list.files()
-for(file in files){GS$createStyle(file = file, name = unlist(strsplit(file, "\\."))[1])}
+files = list.files(pattern = ".xml")
+stylenames = GS$getStyleNames()
+for(file in files){
+	stylename = unlist(strsplit(file, "\\."))[1]
+	if(stylename %in% stylenames){
+		GS$updateStyle(file = file, name = stylename)
+	}else{
+		GS$createStyle(file = file, name = stylename)
+	}
+}
